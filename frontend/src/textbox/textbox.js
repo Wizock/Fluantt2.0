94865c9db9bcc8114ffe4639dbcbf945386844d2
Wizock <36351054+Wizock@users.noreply.github.com>
2021-12-23 18:11:07 +1000

    doing up dragndrop nd dat

--- frontend/src/textbox/textbox.js
+++ frontend/src/textbox/textbox.js
@@ -1,36 +1,45 @@
 import {React, useState} from 'react';
+import { useDrag } from 'react-dnd'
 
 let bufferCounter = 0
 let bufferArray = []
+let changex = 0
+let changey = 0
 
-function TextBox_route(){
+function TextBox_route({ isDragging, text }){
+    
     const [displayData, SetdisplayData] = useState(0);
-    const [counter, setCounter] = useState(0);
+    const [counter    , setCounter    ] = useState(0);
+    const [xdiff      , setxdiff      ] = useState(0);
+    const [ydiff      , setydiff      ] = useState(0);
+
     const createTextBox=(e) => {
         bufferCounter += 1
         let x = e.screenX;
         let y = e.screenY - 70;
 
-        console.log(`x: ${e.screenX}, y: ${e.screenY}`)
-        bufferArray.push(<textarea key={counter.toString()} id={counter} style={{"position": "absolute","left":`${x}px`, "top":`${y}px`}}></textarea>)
+        bufferArray.push(<textarea 
+            onDragStart={
+                e=>{
+                    
+                }
+            } 
+            onDragEnd={
+                e=>{
+                    
+                }
+            } key={counter.toString()} id={counter} style={{"position": "absolute","left":`${x}px`, "top":`${y}px`}}></textarea>)
         bufferArray.map(data => <div>{data}</div>)
-        console.log(bufferArray)
         SetdisplayData(bufferArray)
         setCounter(bufferCounter)
-        console.log(bufferCounter)
-        console.log(displayData)
     }
-    
     return(
-        <div style={{"height":"100vh","width":"100vw"}} onClick = {
-            e => {createTextBox(e)}}>
-
+        <div style={{"height":"100vh","width":"100vw"}} onDoubleClick = {e => {createTextBox(e)}}>
             <div>
                 {displayData}
             </div>
         </div>
     )
 }
-
 
 export default TextBox_route
