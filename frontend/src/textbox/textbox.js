895e1c19ba9be4db28919494b2993537f58f39fa
Wizock <silent.death3500@gmail.com>
2022-01-06 02:26:29 +1000

    need to fix the res changes

--- frontend/src/textbox/textbox.js
+++ frontend/src/textbox/textbox.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import { useDoubleTap } from 'use-double-tap';
 
 
@@ -7,17 +7,35 @@ let bufferArray = []
 let changex = 0
 let changey = 0
 
-function TextBox_route({ isDragging, text }){
-    
+function useWindowDimensions(){
+    function getWIndowDimensions(){
+        const {innerWidth: width, innerHeight: height} = window
+        return { width, height};
+    }
+    const [WinDimensions, setWinDimensions] = useState(getWIndowDimensions);
+    useEffect(()=> {
+        function handleResize(){
+            setWinDimensions(getWIndowDimensions());
+        }
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+        
+    }, []);
+    return WinDimensions;
+}
+
+function TextBox_route(){
+    let {height, width} = useWindowDimensions();
     const [displayData, SetdisplayData] = useState(0);
     const [counter    , setCounter    ] = useState(0);
-    const [xdiff      , setxdiff      ] = useState(0);
-    const [ydiff      , setydiff      ] = useState(0);
-
+    
     const createTextBox=(e) => {
         bufferCounter += 1
-        let x = e.screenX;
-        let y = e.screenY - 70;
+        let x = Math.abs(e.screenX);
+        let y = Math.abs(e.screenY - 70);
+        
+        console.log(`height:${height} width:${width}`)
+        console.log(`x:${x} y:${y}`)
 
         bufferArray.push(<textarea 
             onDragStart={
@@ -35,14 +53,18 @@ function TextBox_route({ isDragging, text }){
         setCounter(bufferCounter)
     }
     const bind = useDoubleTap((event) => {
-        console.log(event.screenX)
-        console.log(event.screenY)
+
         createTextBox(event)
-    });      
+    }); 
+
     return(
-        <div {...bind} style={{"height":"100vh","width":"100vw"}} onDoubleClick = {e => {createTextBox(e)}}>
-            <div>
-                {displayData}
+        
+        <div {...bind} onDoubleClick={e=>{createTextBox(e)}}>
+            <div  style={{"height":height,"width":width}}>
+                <div>
+                    width : {width} ~ height: {height}
+                    {displayData}
+                </div>
             </div>
         </div>
     )
